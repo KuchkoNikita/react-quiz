e18e7732912de63efb15ee91bcdd8c6cb51db687
Nikita Kuchko <kuchko.n.study@gmail.com>
2020-06-20 18:50:59 +0300

    create test list

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -91,6 +91,11 @@ class Quiz extends Component {
     });
   }
 
+  componentDidMount() {
+    console.log('quiz id =', this.props);
+    console.log('quiz id =', this.props.match.params.id);
+  }
+
   render() {
     return (
       <div className={classes.Quiz}>
