477919afef05247c3adace2b96593de8e3dc6b52
Nikita Kuchko <kuchko.n.study@gmail.com>
2020-06-23 15:40:20 +0300

    add styles for  QuizCreator

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,49 @@
 import React, {Component} from 'react';
+import classes from './QuizCreator.css';
+import Button from './../../components/UI/Button/Button.js';
 
 export default class QuizCreator extends Component {
+
+    submitHandler = (event) => {
+        event.preventDefault();
+    }
+
+    addQuestionHadler = () => {
+
+    }
+    
+    createQuizHandler = () => {
+
+    }
+
     render() {
         return (
-            <div>
-                <h1>Quiz Creator</h1>
+            <div className={classes.QuizCreator}>
+                <div>
+                    <h1>Создание теста</h1>
+
+                    <form onSubmit={this.submitHandler}>
+
+                        <input type="text"/>
+                        <hr/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        
+                        <select name="" id=""></select>
+                        
+                        <Button
+                            type="primery"
+                            onClick={this.addQuestionHadler}
+                        >Добавить вопрос</Button>
+
+                        <Button
+                            type="success"
+                            onClick={this.createQuizHandler}
+                        >Cоздать тест</Button>
+                    </form>
+                </div>
             </div>
         )
     }
